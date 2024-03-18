cfdb12675d918e7f88af03ab9e76eec625eaf91c
Srishti168 <101701334+Srishti168@users.noreply.github.com>
2024-03-18 11:10:31 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const app=express();
+app.listen(8080, () => {
+    console.log('server listening on port 8080');
+});
+app.post("/bhfl",(req,resp)=>
+{
+   const numbers=[];
+   const alphabets=[];
+for(const key in req.json["data"])
+  {
+    if(key.isalpha())
+      alphabets.append(key)
+    else
+      numbers.append(key);
+  }
+   resp.send(JSON.stringify(
+   {
+     "is_success":True,
+     "user_id":"Srishti",
+     "email":"srishti1385.be21@chitkara.edu.in",
+     "roll_number":"2110991385",
+     "numbers":numbers,
+     "alphabets":alphabets
+   }))
+})
